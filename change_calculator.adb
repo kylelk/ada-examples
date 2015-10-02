ea0239bab4c16b376d11740ebd2e491a7b394192
kyle kersey <kylelk@comcast.net>
2015-10-02 15:01:23 -0700

    fixed change calculator

--- change_calculator.adb
+++ change_calculator.adb
@@ -17,6 +17,7 @@ procedure Change_Calculator is
    Currency_Names  : array (1..10) of Currency_Denomination;
    Currency_Counts : array (1..10) of Integer := (others=>0);
    Currency_Index  : Integer range 1..10;
+
    procedure Get_Money_Prompt(Amount: out Money) is
       Response : String(1..20);
       Last     : Natural;
@@ -35,6 +36,7 @@ procedure Change_Calculator is
          end;
       end loop;
    end Get_Money_Prompt;
+
 begin
    Currency_Names := (("Penny     ", 0.01),
                       ("Nickle    ", 0.05),
@@ -46,13 +48,12 @@ begin
                       ("20 Dollar ", 20.00),
                       ("50 Dollar ", 50.00),
                       ("100 Dollar", 100.00));
+
    Get_Money_Prompt(Input_Amount);
-   --while Input_Amount > 0.00 loop
+
    for I in Currency_Names'Range loop
       Currency_Index := (Currency_Names'Length-I)+1;
-      -- Money_IO.Put(Currency_Names(Currency_Index).Value, 0);
-      -- Text_IO.New_Line;
-      while Input_Amount > Currency_Names(Currency_Index).Value loop
+      while Input_Amount >= Currency_Names(Currency_Index).Value loop
          Input_Amount := Input_Amount - Currency_Names(Currency_Index).Value;
          Currency_Counts(I) := Currency_Counts(I) + 1;
       end loop;
@@ -60,10 +61,11 @@ begin
 
    for I in Currency_Counts'Range loop
       Currency_Index := (Currency_Names'Length-I)+1;
-      Text_IO.Put(Currency_Names(I).Name & " ");
-      Integer_Text_IO.Put(Currency_Counts(Currency_Index));
-      Text_IO.New_Line;
+      if Currency_Counts(Currency_Index) /= 0 then
+        Text_IO.Put(Currency_Names(I).Name & " ");
+        Integer_Text_IO.Put(Currency_Counts(Currency_Index));
+        Text_IO.New_Line;
+      end if;
    end loop;
 
-   -- Money_IO.Put(Input_Amount, 0);
 end Change_Calculator;
